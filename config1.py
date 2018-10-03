41c02bafcda94a24958f538b714d553cda4dc6e6
Ruby <Robina@ucsd.edu>
2018-10-02 23:18:24 -0700

    Mars Facts, Weather added

---
+++ config1.py
@@ -0,0 +1,6 @@
+# These keys are not valid. Use your own API keys
+
+consumer_key = "xGaknYVONq0fcr1jj4GOjKsPO"
+consumer_secret = "J4djTsjFTurPYlbyGPjyeMe8vtP1xZL9f9FNiWvwTiJk8MeFuj "
+access_token = "YOUR KEY HERE"
+access_token_secret = "YOUR KEY HERE"
